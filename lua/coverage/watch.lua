3d64405de9f5b40ad3c662449fd0d1cc20deedcf
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-11 13:07:02 +0400

    chore: pre-merge fixes
    - watch.lua: use vim.uv with vim.loop fallback for Neovim 0.9 compat
    - doc/tags: generate helptags for :help nvim-coverage
    - flake.lock: pin nixpkgs and flake-utils for reproducible CI

--- lua/coverage/watch.lua
+++ lua/coverage/watch.lua
@@ -2,6 +2,9 @@ local M = {}
 
 local config = require("coverage.config")
 
+-- vim.uv is the preferred alias in Neovim 0.10+; fall back to uv for 0.9
+local uv = vim.uv or uv
+
 local fs_event = nil
 local debounce_timer = nil
 
@@ -31,20 +34,20 @@ start = function(fname, change_cb, events)
         change_cb()
     end
 
-    fs_event = vim.loop.new_fs_event()
+    fs_event = uv.new_fs_event()
     ---@diagnostic disable-next-line: unused-local
-    vim.loop.fs_event_start(fs_event, fname, { watch_entry = false, stat = false, recursive = false },
+    uv.fs_event_start(fs_event, fname, { watch_entry = false, stat = false, recursive = false },
         function(err, filename, ev)
             if err then
                 vim.notify("Coverage watch error: " .. err, vim.log.levels.ERROR)
                 M.stop()
             elseif ev.rename then
-                if debounce_timer ~= nil then vim.loop.timer_stop(debounce_timer) end
+                if debounce_timer ~= nil then uv.timer_stop(debounce_timer) end
                 debounce_timer = vim.defer_fn(function()
                     start(fname, change_cb, ev)
                 end, 0)
             else
-                if debounce_timer ~= nil then vim.loop.timer_stop(debounce_timer) end
+                if debounce_timer ~= nil then uv.timer_stop(debounce_timer) end
                 debounce_timer = vim.defer_fn(function()
                     debounce_timer = nil
                     change_cb()
@@ -61,7 +64,7 @@ M.start = start
 --- Stops the file watcher.
 M.stop = function()
     if fs_event ~= nil then
-        vim.loop.fs_event_stop(fs_event)
+        uv.fs_event_stop(fs_event)
     end
     fs_event = nil
 end
